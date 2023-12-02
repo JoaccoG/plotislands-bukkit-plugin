393f241943a2ac672a41934967b01c84536a804a
JoaccoG <joaquingodoy2407@gmail.com>
2023-12-02 22:14:27 +0100

    Add prefix and version variables to console messages

--- src/main/java/org/joaccog/PlotIslands.java
+++ src/main/java/org/joaccog/PlotIslands.java
@@ -5,20 +5,25 @@ import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class PlotIslands extends JavaPlugin {
+    public static String prefix = "&8[&7PlotIslands&8]";
+    private final String version = getDescription().getVersion();
 
     public void onEnable() {
         Bukkit
                 .getConsoleSender()
                 .sendMessage(
-                        ChatColor.translateAlternateColorCodes('&', "&6Enabling PlotIslands v...")
+                        ChatColor.translateAlternateColorCodes(
+                                '&', String.format("%s &eEnabled successfully (v%s)", prefix, version)
+                        )
                 );
     }
-
     public void onDisable() {
         Bukkit
                 .getConsoleSender()
                 .sendMessage(
-                        ChatColor.translateAlternateColorCodes('&', "&6Disabling PlotIslands v...")
+                        ChatColor.translateAlternateColorCodes(
+                                '&', String.format("%s &eDisabled successfully (v%s)", prefix, version)
+                        )
                 );
     }
 
