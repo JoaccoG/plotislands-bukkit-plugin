55df1437b99312c9df3842f1401b6fc3cee5dc57
JoaccoG <joaquingodoy2407@gmail.com>
2023-12-06 21:28:12 +0100

    Replace messages calls with log calls

--- src/main/java/org/joaccog/PlotIslands.java
+++ src/main/java/org/joaccog/PlotIslands.java
@@ -1,11 +1,5 @@
 package org.joaccog;
 
-import java.io.File;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.Objects;
-import java.util.Set;
-
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
@@ -15,7 +9,13 @@ import org.bukkit.event.world.WorldInitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import org.joaccog.commands.MainCommand;
-import org.joaccog.utils.MessageUtils;
+import org.joaccog.utils.LogUtils;
+
+import java.io.File;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Objects;
+import java.util.Set;
 
 public class PlotIslands extends JavaPlugin implements Listener {
     public static String prefix = "&8[&7PlotIslands&8] ";
@@ -29,32 +29,33 @@ public class PlotIslands extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         try {
-            getLogger().info("Attempting to get plugin's folder");
+            LogUtils.info("Registering internal commands...");
+            registerCommands();
+
+            LogUtils.info("Getting plugin configuration...");
             dataFolder = getDataFolder();
             databaseFile = new File(dataFolder, "database.yml");
             config = YamlConfiguration.loadConfiguration(databaseFile);
             processedWorlds.addAll(config.getStringList("processedWorlds"));
+
+            LogUtils.info("Listening worlds creation events...");
+            Bukkit
+                .getPluginManager()
+                .registerEvents(this, this);
+
+            LogUtils.info("Successfully enabled on v" + pluginVersion);
         } catch (Exception e) {
-            getLogger().severe("Error while getting plugin's folder");
-            getLogger().severe("Disabling " + pluginName);
+            LogUtils.severe("Error while enabling " + pluginName + " v" + pluginVersion);
+            LogUtils.severe("Error stack trace: \n" + e.getMessage());
+            LogUtils.severe("Disabling...");
             onDisable();
         }
-
-        Bukkit.getPluginManager().registerEvents(this, this);
-
-        registerCommands();
-        Bukkit
-            .getConsoleSender()
-            .sendMessage(MessageUtils.getColoredMessageWithPrefix("&eEnabled successfully v" + pluginVersion));
     }
 
     @Override
     public void onDisable() {
-        config.set("processedWorlds", new LinkedList<>(processedWorlds));
-        saveConfig();
-        Bukkit
-            .getConsoleSender()
-            .sendMessage(MessageUtils.getColoredMessageWithPrefix("&eDisabled successfully v" + pluginVersion));
+        saveProcessedWorlds();
+        LogUtils.severe("Successfully disabled on v" + pluginVersion);
     }
 
     public void registerCommands() {
@@ -63,28 +64,35 @@ public class PlotIslands extends JavaPlugin implements Listener {
             .setExecutor(new MainCommand());
     }
 
+    public void saveProcessedWorlds() {
+        config.set("processedWorlds", new LinkedList<>(processedWorlds));
+        saveConfig();
+        LogUtils.info("List of worlds on database updated.");
+    }
+
     @EventHandler
     public void onWorldCreation(WorldInitEvent event) {
         String worldName = event.getWorld().getName();
 
         if (!processedWorlds.contains(worldName)) {
             String customCommand = "vt toggle " + worldName;
-            getLogger().info("Command stored, will try to execute it on: " + worldName);
 
             Bukkit.getScheduler().runTaskLater(this, () -> {
                 try {
-                    getLogger().info("Adding world: " + worldName + " to database");
-                    processedWorlds.add(worldName);
-                    getLogger().info("Executing custom commands for world: " + worldName);
+                    LogUtils.info("Executing custom commands for world: " + worldName);
                     Bukkit.dispatchCommand(Bukkit.getConsoleSender(), customCommand);
-                    getLogger().info("VT+ toggled for world: " + worldName);
+                    LogUtils.info("Successfully executed all commands.");
+                    LogUtils.info("Saving world on database");
+                    processedWorlds.add(worldName);
+                    saveProcessedWorlds();
+                    LogUtils.info("Successfully saved world on database.");
                 } catch (Exception e) {
-                    e.printStackTrace();
-                    getLogger().severe("Error toggling VT+ for world: " + worldName);
+                    LogUtils.severe("Unexpected error while executing commands on world: " + worldName);
+                    LogUtils.severe("Error stack trace: \n" + e.getMessage());
                 }
             }, 100L);
         } else {
-            getLogger().info("Skipping ");
+            LogUtils.warning("Skipping commands execution on world: " + worldName);
         }
     }
 }
